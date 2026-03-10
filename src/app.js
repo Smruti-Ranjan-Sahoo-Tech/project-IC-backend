3cf48ca26d34a94086a04c2b194f9de84ce63ac8
Smruti Ranjan Sahoo <68041717+Smruti-Ranjan-Sahoo-Tech@users.noreply.github.com>
2026-03-10 11:57:17 +0530

    11.48 am

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(methodOverride('_method'));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true
 }));
 
